2242be706328fad8d515156bee9acd0c694d990a
Gerald R Patterson <jerripatterson@gmail.com>
2022-10-25 13:16:12 -0500

    Update to program Enter file to update:IDLe Projects

---
+++ IDLE.py
@@ -0,0 +1,19 @@
+Python 3.10.6 (main, Aug 10 2022, 11:40:04) [GCC 11.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+def square(x):
+    return x * x
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
+
+    
+The square of 0 is 0
+The square of 1 is 1
+The square of 2 is 4
+The square of 3 is 9
+The square of 4 is 16
+The square of 5 is 25
+The square of 6 is 36
+The square of 7 is 49
+The square of 8 is 64
+The square of 9 is 81
