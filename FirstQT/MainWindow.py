2fd1948ffb41926a5628ec9b3481d54646b98102
jerripat <jerripatterson@gmail.com>
2022-12-20 20:15:32 -0600

    Redesign QTProjects

--- FirstQT/MainWindow.py
+++
@@ -1,8 +0,0 @@
-# This Python file uses the following encoding: utf-8
-from PyQt5 import QtCore
-from PyQt5 import QtWidgets
-
-
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        pass
